832429d2285980f0b7a372090014afc487c96dcb
Arijit Basu <sayanarijit@gmail.com>
2022-02-02 15:06:21 +0530

    Use IntoResponse

--- src/route/events.rs
+++ src/route/events.rs
@@ -1,13 +1,13 @@
 use crate::events;
 use crate::result::Result;
 use axum::extract::Extension;
-use axum::response::Json;
+use axum::response::{IntoResponse, Json};
 use sea_orm::entity::prelude::*;
 use sea_orm::QueryOrder;
 
 pub async fn list(
     Extension(ref db): Extension<DatabaseConnection>,
-) -> Result<Json<Vec<events::Model>>> {
+) -> Result<impl IntoResponse> {
     let events = events::Entity::find()
         .order_by_asc(events::Column::Title)
         .all(db)
